9db262baf75068ca8ddab9be219391b83b0f92be
Cindy Pang <cipang@coca-cola.com>
2018-08-12 19:55:10 -0400

    fixed path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,13 +8,13 @@ function htmlRoutes(app) {
 
     // A GET Route to /survey which should display the survey page.
     app.get('/survey', function (req, res) {
-      // res.sendFile(path.join(__dirname + '/../public/survey.html'));
-      res.sendFile(path.join(__dirname + '../public/survey.html'));
+      res.sendFile(path.join(__dirname + '/../public/survey.html'));
+      // res.sendFile(path.join(__dirname + '../public/survey.html'));
     });
   
     // A default USE route that leads to home.html which displays the home page.
     app.use(function (req, res) {
-      res.sendFile(path.join(__dirname + '../public/home.html'));
+      res.sendFile(path.join(__dirname + '/../public/home.html'));
     });
   
   }
